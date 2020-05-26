ca5f8cb5262b84829eac8167849bff42c92897bf
Alexandre Lemaitre <alexandre.lemaitre2@gmail.com>
2020-05-26 18:44:24 +0200

    add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+
+cosmopolitan = Cocktail.create(name: "Cosmopolitan")
+mm = Cocktail.create(name: "Moscow Mule")
+Cocktail.create(name: "Jaeger Bomb")
+
+dr = Ingredient.create(name: "Dark rum")
+sc = Ingredient.create(name: "Strawberry schnapps")
+sco = Ingredient.create(name: "Scotch")
+
+Dose.create(description: "2 cl", cocktail: cosmopolitan, ingredient: sco)
+Dose.create(description: "2 spoons of ginger", cocktail: mm, ingredient: sc)
+Dose.create(description: "2 spoons of lemon")
+
+
